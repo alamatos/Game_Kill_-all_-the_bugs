4b23fb6dd4dfd6bd54113da086ed17278e805893
Alamatos <alamatos@gmail.com>
2019-05-09 21:58:57 +0100

    Last game commit

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 var actualWidth = 0
 var actualHeight = 0
+var lives = 1
+var time = 15
+var createBug = 1500
+
+
+
+function startGame() {
+        
+    var levelChoice= document.getElementById('LevelBox').value
+
+        if (levelChoice =='SelectLevel') {
+            alert('Please insert a level for the game')
+            return false
+        }   else {
+            window.location.href='killingZone.html?' + levelChoice
+        }
+            }
+
+    var level = window.location.search
+
+    level = level.replace('?', '')
+
+        if (level==='Easy') {
+            createBug = 2000
+        } else if (level ==='Medium') {
+            createBug = 1500
+        } else {
+            createBug = 1000
+        }
 
 function gameArea () {
     actualWidth = window.innerWidth
@@ -13,7 +42,43 @@ function gameArea () {
 gameArea()
 
 
+
+// i need to reduce the time available in the timer, in order to know if the user wins or loses the game
+var timer = setInterval(function () {
+    time--
+
+if (time<0) {
+    clearInterval(timer)
+    clearInterval(placeBug)
+    window.location.href = 'victory.html'
+
+} else {
+    document.getElementById('timer').innerHTML=time 
+}
+},1000)
+
 function randomPositioning () {
+
+// I already implemented a timer to position a bug in the killing zone,
+// but i only want to have one bug at a time, so
+// I need to check if a bug is already on the killing zone.
+
+if (document.getElementById('bug')) {
+    document.getElementById('bug').remove()
+
+
+// Now i need to withdraw a life per bug not killed
+if (lives>3) {
+
+    window.location.href ='GameOver.html'
+
+} else{
+
+    document.getElementById('L' + lives).src= 'images/empty_heart.png'
+    lives ++
+}
+}
+
 // Create random positions for the bugs, having in consideration the actual size of the widow, as well as
 // the dimension of the picture used to represent the bug (that's why i decreased 90 px) 
 
@@ -37,6 +102,11 @@ function randomPositioning () {
     bug.style.left= positionX + 'px'
     bug.style.top= positionY + 'px'
     bug.style.position= 'absolute'
+    bug.id='bug'
+    bug.onclick= function () {
+        this.remove()
+        
+    }
 
     document.body.appendChild(bug)   
 
@@ -77,3 +147,4 @@ function chooseSide () {
         return 'rightSide'
     }
 }
+
